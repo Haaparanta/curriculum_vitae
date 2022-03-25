c2d1040e5a0fa27c59847b27ca9f02e15d590202
vesa haaparanta <vesahaaparanta@hotmail.fi>
2022-03-26 00:57:04 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # curriculum_vitae
+
+//Hi, I am Vesa Haaparanta and I study third-year computer science at the university of tampere on the Hervanta campus. I am fluent in Finnish and English.
+
+//I have a strong passion for web development and I am looking forward to learning more about it. More specifically I would like to be frontend, mobile and full-stack developer.
+
+//I am an open-minded person, who is always willing to learn new things. I am also a team player, who is always willing to help others.
+
+//My main interests are:
+
+//* Frontend development
+//* Full-stack development
+//* Mobile development
+//* Game development
+//* Artificial intelligence
+//* Machine learning
+
+//I have a strong background in software development and I am familiar with the following technologies:
+
+//* C++
+//* Python
+//* JavaScript
+//* HTML
+//* CSS
+//* React
+//* React-Native
+//* Flutter
+//* Dart
+//* Node.js
+//* MySQL
+//* NoSQL
+//* REST
+//* Git
+//* Blender
+//* Figma
+//* Illustrator
+//* SQF
+
+
--- cv/src/App.js
+++ cv/src/App.js
@@ -28,20 +28,15 @@ const analytics = firebase.analytics();
 
 
 
-
-
-
-
 function App() {
   const [user] = useAuthState(auth);
   const [pageNumber, setPageNumber] = useState(0);
   console.log(pageNumber)
 
-
   if (pageNumber === 0) {
     return (
       <div>
-        <Navbar>
+         <Navbar>
           <li className="nav-item">
             <button className="icon-button" onClick={() => setPageNumber(0)}>
               {<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M575.8 255.5C575.8 273.5 560.8 287.6 543.8 287.6H511.8L512.5 447.7C512.5 450.5 512.3 453.1 512 455.8V472C512 494.1 494.1 512 472 512H456C454.9 512 453.8 511.1 452.7 511.9C451.3 511.1 449.9 512 448.5 512H392C369.9 512 352 494.1 352 472V384C352 366.3 337.7 352 320 352H256C238.3 352 224 366.3 224 384V472C224 494.1 206.1 512 184 512H128.1C126.6 512 125.1 511.9 123.6 511.8C122.4 511.9 121.2 512 120 512H104C81.91 512 64 494.1 64 472V360C64 359.1 64.03 358.1 64.09 357.2V287.6H32.05C14.02 287.6 0 273.5 0 255.5C0 246.5 3.004 238.5 10.01 231.5L266.4 8.016C273.4 1.002 281.4 0 288.4 0C295.4 0 303.4 2.004 309.5 7.014L564.8 231.5C572.8 238.5 576.9 246.5 575.8 255.5L575.8 255.5z"/></svg>}
@@ -102,6 +97,7 @@ function App() {
             </button>
           </li>
         </Navbar>
+        <Study/>
       </div>
     );
   } else if (pageNumber === 2) {
@@ -230,6 +226,9 @@ function Promo() {
   );
 }
 
+
+
+
 function Navbar(props) {
   return (
     <nav className="navbar">
@@ -238,10 +237,19 @@ function Navbar(props) {
   );
 }
 
+function Study() {
+  return (
+    <div className='study'>
+
+    </div>
+  );
+}
+
 function Projects() {
   return (
-    <>
-    </>
+   <div className='project'>
+
+   </div>
   );
 }
 
