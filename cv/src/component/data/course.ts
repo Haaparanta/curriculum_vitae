870a2598dfb274f4b56346455c8c7ea0e00b5a9c
vesa haaparanta <vesahaaparanta@hotmail.fi>
2022-04-06 10:16:23 +0300

    added more content to course.ts

--- cv/src/component/data/course.ts
+++ cv/src/component/data/course.ts
@@ -5,7 +5,22 @@ const Courses: Course[] = [
   {
     id: 1,
     title: 'Full Stack Open 2022',
-    description: 'I have learned so much of js. ts, css, html and lots of more.'
+    description: 'I have learned so much of JavaScript TypeScript, CSS, HTML and lots of more.'
+  },
+  {
+    id: 2,
+    title: 'Programming 3: Techniques',
+    description: 'I learned so much about C++ and Qt. I learned how to manage big projects from nysse project.'
+  },
+  {
+    id: 3,
+    title: 'Introduction to Software Engineering',
+    description: 'I learned about the basics of software engineering.'
+  },
+  {
+    id: 4,
+    title: 'Data Structures and Algorithms',
+    description: 'I learned about the basics of data structures and algorithms.'
   },
 ];
 
